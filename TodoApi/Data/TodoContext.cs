c981968c04339f51ca4a2850642abf63f3a2ecc8
MrWest <kidwest87@gmail.com>
2025-08-11 15:42:17 -0400

    feat: TodoListItem CRUD first approach

--- TodoApi/Data/TodoContext.cs
+++ TodoApi/Data/TodoContext.cs
@@ -7,4 +7,6 @@ public class TodoContext : DbContext
         : base(options) { }
 
     public DbSet<TodoList> TodoList { get; set; } = default!;
+    public DbSet<TodoListItem> TodoListItem { get; set; } = default!;
+    
 }
---
+++ TodoApi/Dtos/CreateTodoListItem.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.Dtos;
+
+public class CreateTodoListItem
+{
+    public required string Name { get; set; }
+
+    public required string Description { get; set; }
+}
