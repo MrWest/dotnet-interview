5bcc862009c8d8cbda83d46a6cfb02055ee6306f
MrWest <kidwest87@gmail.com>
2025-08-11 19:49:36 -0400

    feat: TodoListItem collection migration

---
+++ TodoApi/Migrations/20250811231113_AddTodoListItem.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTodoListItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoListItems_TodoList_TodoListId",
+                table: "TodoListItems");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TodoListId",
+                table: "TodoListItems",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoListItems_TodoList_TodoListId",
+                table: "TodoListItems",
+                column: "TodoListId",
+                principalTable: "TodoList",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoListItems_TodoList_TodoListId",
+                table: "TodoListItems");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TodoListId",
+                table: "TodoListItems",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoListItems_TodoList_TodoListId",
+                table: "TodoListItems",
+                column: "TodoListId",
+                principalTable: "TodoList",
+                principalColumn: "Id");
+        }
+    }
+}
--- TodoApi/Models/TodoListItem.cs
+++ TodoApi/Models/TodoListItem.cs
@@ -3,13 +3,10 @@ namespace TodoApi.Models;
 public class TodoListItem
 {
     public long Id { get; set; }
-
+    public long TodoListId { get; set; }
     public TodoList? TodoList { get; set; }
     public required string Name { get; set; }
-
-    public  bool Completed { get; set; }
-
-     public  int Progress { get; set; }
-
+    public  bool Completed { get; set; } = false;
+     public  int Progress { get; set; } = 0;
     public required string Description { get; set; }
 }
